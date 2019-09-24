377a15845db26caaf195f5369cf84bcbdc4cd176
THUMarkLau <liuxuxin@outlook.com>
2019-09-24 14:27:24 +0800

    build environment by Visual Studio 2019

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+	*
+	* Description:使用OpenCV实现一个SFM的Demo，并在此基础上进行改进
+	* Author: 刘旭鑫
+	* Date: 2019.9.23
+	* IDE: Visual Studio 2019
+	* OpenCV Version: 4.1.1
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+
+int main()
+{
+	return 0;
+}
